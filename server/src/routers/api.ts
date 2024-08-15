99ef95c6cc39bf88462c3b02aa5d8fd08829b5b8
Shaul1KR <shaulikr07@gmail.com>
2024-08-15 10:01:34 +0300

    Added domain api

--- server/src/routers/api.ts
+++ server/src/routers/api.ts
@@ -1,8 +1,10 @@
 import express from "express";
 import publisher from "./publisher";
+import domain from "./domain";
 
 const router = express.Router();
 
 router.use("/publishers", publisher);
+router.use("/domains", domain);
 
 export default router;
